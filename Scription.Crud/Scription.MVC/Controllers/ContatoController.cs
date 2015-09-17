bc93bf897ae12cac9092cdb6bf2a24a3810921d5
Luiz Fernando <luiz_fernando171@yahoo.com.br>
2015-09-17 19:14:02 -0300

    Primeiro commit

---
+++ Scription.Crud/Scription.MVC/Controllers/ContatoController.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scription.MVC.Models;
+using System.Data;
+
+namespace Scription.MVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private EntidadesBD banco = new EntidadesBD();
+
+
+        public ActionResult Index()
+        {
+            var contatos = banco.Contato.ToList();
+
+            return View(contatos);
+
+        }
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                bool teste = banco.Contato.ToList().Exists(c => c.Nome == contato.Nome);
+                if (!banco.Contato.ToList().Exists(c => c.Nome == contato.Nome))
+                {
+                    banco.Contato.Add(contato);
+                    banco.SaveChanges();
+
+                    return RedirectToAction("Index");
+
+                }
+                else
+                    return View(contato);
+            }
+
+            return View(contato);
+        }
+        public ActionResult Editar(long id)
+        {
+            if (id <= 0 )
+                return RedirectToAction("Index");
+            else
+            {
+                var contato = banco.Contato.Find(id);
+
+                return View(contato);
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Contato contato)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    banco.Entry(contato).State = EntityState.Modified;
+
+                    banco.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+             }catch(Exception e){
+
+                 throw new UpdateException(e.Message);
+             }
+            return View(contato);
+        }
+
+        
+        public ActionResult Excluir(long id)
+        {
+            try
+            {
+                Contato contato = banco.Contato.Find(id);
+                
+                if (contato != null)
+                {
+
+                    banco.Contato.Remove(contato);
+                    banco.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                throw new UpdateException(ex.StackTrace); 
+            }
+        }
+     }
+}
---
+++ Scription.Crud/Scription.MVC/Models/ContatoMetadado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Scription.MVC.Models
+{
+    [MetadataType(typeof(ContatoMetadado))]
+    public partial class Contato
+    {   
+
+    }
+
+    public class ContatoMetadado
+    {
+        //[RegularExpression()]
+        [Required (ErrorMessage="Obrigatorio Informa o Nome")]
+        [StringLength(100, ErrorMessage="O nome deve possuir no maximo 100 caracteres")]
+        [RegularExpression("^([a-zA-Z0-9 .&'-]+)$", ErrorMessage = "O nome nao pode conter caracteres especiais")]
+        public string Nome { get; set; }
+        
+        //[RegularExpression()]
+        [Required(ErrorMessage= "Obrigatorio informar o Email")]
+        [RegularExpression("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9]+)*\\.([a-z]{2,4})$", ErrorMessage = "Email invalido")]
+        public string Email { get; set; }
+
+        //[RegularExpression()]
+        [Required(ErrorMessage = "Obrigatorio Informa o Telefone")]
+        [StringLength(14, ErrorMessage = "O telefone deve possuir no maximo 14 caracteres")]
+        public string Telefone { get; set; }
+
+    }
+}
